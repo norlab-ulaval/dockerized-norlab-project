92ab0ca8f0fb8cbbf9a4f073234deefefa3b1ccb
RedLeader962 <redleader962@gmail.com>
2025-08-19 23:11:13 -0400

    fix: clarify dotenv loading and add note for DNA internal file creation
    - Fix typo preventing dotenv file creation
    - Moved shellcheck suppression closer to the `source` command for improved
      readability and context.
    - Added documentation indicating that `.env.dna-internal.local` is required
      by specific Docker Compose configurations, ensuring clear guidance for
      users in case the file is missing.
    - Enhances maintainability by documenting key setup requirements directly
      in the script.
    
    Issue NMO-779

--- src/lib/core/utils/load_super_project_config.bash
+++ src/lib/core/utils/load_super_project_config.bash
@@ -145,8 +145,8 @@ function dna::load_super_project_configurations() {
         n2st::print_msg_warning "Be advised, sourcing super project ${MSG_EMPH_FORMAT}local${MSG_END_FORMAT} dotenv file ${MSG_DIMMED_FORMAT}${super_project_dotenv_local}${MSG_END_FORMAT}."
     fi
     # Note: super project dotenv local should be source after the super project main dotenv
-    # shellcheck disable=SC1090
     set -o allexport
+    # shellcheck disable=SC1090
     source "${super_project_dotenv_local}" || return 1
     set +o allexport
   fi
@@ -164,7 +164,8 @@ function dna::load_super_project_configurations() {
     source "${dna_internal_local}" || return 1
     set +o allexport
   else
-    cat "${dna_internal_local}" << EOF
+    # Note: Dotenv file .env.dna-internal.local is required by docker-compose.project.[build|run].*.yaml files
+    cat > "${dna_internal_local}" << EOF
 # =================================================================================================
 # Set Dockerized-NorLab project application (DNA) internal environment variable LOCALY.
 #
